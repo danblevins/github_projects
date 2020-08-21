d2504a0fb504c9d7654d7424584584feeb3ade9c
danblevins <danblevins@Dans-MacBook-Pro.local>
2020-08-20 19:15:51 -0700

    Included GraphQL

--- WebDevelopment/ImHungry/main.py
+++ WebDevelopment/ImHungry/main.py
@@ -2,49 +2,93 @@ from flask import Flask, redirect, url_for, render_template, request, json
 import pandas as pd
 import numpy as np
 from yelpapi import YelpAPI
+#from flask_graphql import GraphQLView
+#from schema import schema
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
 from bs4 import BeautifulSoup
 import requests
 import os
-key = YelpAPI(os.environ.get('yelp_key'))
+#
 
 app=Flask(__name__)
 
 ### Web Pages ###
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    try:
-        result=[]
-        food = request.form.get("Food", 'Food')
-        location = request.form.get("Location", 'City, State or Zipcode')
+    result=[]
+    food = request.form.get("Food", 'Food')
+    location = request.form.get("Location", 'City, State or Zipcode')
 
-        if food in ['Food',''] and location in ['City, State or Zipcode','']:
-            result=''
-            message=''
-        else:
-            store = key.search_query(term=food, location=location, radius=40000, limit=10)
+    if food in ['Food',''] and location in ['City, State or Zipcode','']:
+        result=''
+        message=''
+    else:
+        try:
+            message = 'View your results below! (Results loaded using GraphQL)'
+            api_key = os.environ.get('yelp_key')
+            header = {'Authorization': 'bearer {}'.format(api_key),
+                    'Content-Type':"application/json"}
+            transport = RequestsHTTPTransport(url='https://api.yelp.com/v3/graphql', headers=header, use_json=True)
+            client = Client(transport=transport, fetch_schema_from_transport=True)
+
+            gql_str = ("""{{search(term: "{}",
+                            location: "{}",
+                            radius: 40000,
+                            limit: 10,
+                            sort_by: "best_match",
+                            open_now: true) {{
+                        business {{
+                            name
+                            price
+                            rating
+                            review_count
+                            location {{
+                                address1
+                                city
+                                state
+                                country
+                            }}
+                            categories {{
+                                alias
+                            }}
+                        }}
+                    }}
+                }}
+            """).format(food, location)
+            query_dict = client.execute(gql(gql_str))
+
+            for i in range(len(query_dict['search']['business'])):
+                temp_dict={}
+                temp_dict['name'] = query_dict['search']['business'][i]['name']
+                temp_dict['price'] = query_dict['search']['business'][i]['price']
+                temp_dict['rating'] = query_dict['search']['business'][i]['rating']
+                temp_dict['num_rating'] = query_dict['search']['business'][i]['review_count']
+                temp_dict['address'] = (
+                    query_dict['search']['business'][i]['location']['address1'] + ', ' 
+                    + query_dict['search']['business'][i]['location']['city'] + ', ' 
+                    + query_dict['search']['business'][i]['location']['state'] + ', ' 
+                    + query_dict['search']['business'][i]['location']['country']
+                )
+                temp_list=[]
+                for j in range(len(query_dict['search']['business'][i]['categories'])):
+                    try:
+                        temp_list.append(query_dict['search']['business'][i]['categories'][j]['alias'])
+                        temp_dict['category'] = ' / '.join(temp_list)
+                    except:
+                        temp_dict['category'] = 'N/A'
+                result.append(temp_dict)
+
+        except:
+            key = YelpAPI(os.environ.get('yelp_key'))
+            store = key.search_query(term=food, location=location, radius=40000, limit=10, sort_by='best_match', open_now=True)
             for i in range(len(store['businesses'])):
                 temp_dict={}
                 temp_dict['name'] = store['businesses'][i]['name']
-                try:
-                    temp_dict['price'] = store['businesses'][i]['price']
-                except:
-                    temp_dict['price'] = 'Unavailable'
-                    continue
-                try: 
-                    temp_dict['rating'] = store['businesses'][i]['rating']
-                except:
-                    temp_dict['rating'] = 'Unavailable'
-                    continue
-                try:
-                    temp_dict['num_rating'] = store['businesses'][i]['review_count']
-                except:
-                    temp_dict['num_rating'] = 'Unavailable'
-                    continue
-                try:
-                    temp_dict['address'] = ', '.join(store['businesses'][i]['location']['display_address'])
-                except:
-                    temp_dict['address'] = 'Unavailable'
-                    continue
+                temp_dict['price'] = store['businesses'][i]['price']
+                temp_dict['rating'] = store['businesses'][i]['rating']
+                temp_dict['num_rating'] = store['businesses'][i]['review_count']
+                temp_dict['address'] = ', '.join(store['businesses'][i]['location']['display_address'])
 
                 temp_list=[]
                 for j in range(len(store['businesses'][i]['categories'])):
@@ -52,14 +96,11 @@ def index():
                         temp_list.append(store['businesses'][i]['categories'][j]['title'])
                         temp_dict['category'] = ' / '.join(temp_list)
                     except:
-                        temp_dict['category'] = 'Unavailable'
+                        temp_dict['category'] = 'N/A'
                         continue
                 result.append(temp_dict)
 
-            message = 'View your results below'
-
-    except:
-        pass
+            message = message = 'View your results below! (Results loaded using REST API)'
 
     return render_template("index.html", result=result, food=food, location=location, message=message)
 
